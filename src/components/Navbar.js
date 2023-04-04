6fa152401b8125c8379f9d70b77cc21979736ac7
Nitish <nk301998@gmail.com>
2023-04-05 03:16:14 +0530

    Navbar items logo and styling added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import logo from '../images/Insta_logo.png'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img src={logo} alt="" />
+        <ul className='nav-menu'>
+            <li>SignUp</li>
+            <li>SignIn</li>
+            <li>Profile</li>
+        </ul>
+    </div>
   )
 }
