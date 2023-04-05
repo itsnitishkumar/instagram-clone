011a1e690b961dd4521e43e29e581fececfa5bab
Nitish <nk301998@gmail.com>
2023-04-06 02:32:13 +0530

    added Home, Profile, Signin, Signup components alongwith routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
  import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter,Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/signin' element={<SignIn/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
