bfa81f954a76c5f4ae17860ea6871f874e2de702
Nitish <nk301998@gmail.com>
2023-04-13 14:17:55 +0530

    Designing of Home and Profile page (only frontend)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      {/* card */}
+      <div className="card">
+        {/* card header */}
+          <div className="card-header">
+            <div className="card-pic">
+              <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+            </div>
+            <h5>Ramesh</h5>
+          </div>
+          
+          {/* card image */}
+          <div className="card-image">
+            <img src="https://images.unsplash.com/photo-1680955886616-55b83de7489f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" /> 
+          </div>
+
+          {/* card content */}
+          <div className="card-content">
+            <span className="material-symbols-outlined">
+              favorite
+            </span>
+            <p>1 Like</p>
+            <p>This is amazing</p>
+          </div>
+
+          {/* add comments */}
+          <div className="add-comment">
+            <span className="material-symbols-outlined">
+              Mood
+            </span>
+            <input type="text" name="" id="" placeholder='Add a comment'/>
+            <button className="comment">Post</button>
+          </div>
+
+        </div>
+      </div>
   )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import './Profile.css'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      {/* profile frame */}
+      <div className="profile-frame">
+        <div className="profile-pic">
+          <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        </div>
+        <div className="profile-data">
+          <h1>Canta Coder</h1>
+          <div className="profile-info" style={{display: "flex"}}>
+            <p>40 posts</p>
+            <p>40 followers</p>
+            <p>40 following</p>
+          </div>
+        </div>
+      </div>
+
+      <hr style={{width: "90%", margin: "auto", opacity:"0.8", margin: "25px auto"}}/>
+
+      {/* Gallery */}
+      <div className="gallery">
+        <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+        <img src="https://plus.unsplash.com/premium_photo-1664302511310-a0fd2e0cfead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29uJTIwc3F1YXJlfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+      </div>
+
+    </div>
   )
 }
